ecb539e47c1a1f4ac5de730c3560c3b0eab3d3f7
TR <thirusharamjee@outlook.com>
2024-12-06 11:06:42 +0200

    updates for ex 3

--- src/ContosoSuitesWebAPI/Services/VectorizationService.cs
+++ src/ContosoSuitesWebAPI/Services/VectorizationService.cs
@@ -1,4 +1,5 @@
-﻿using Azure.AI.OpenAI;
+﻿using System.Globalization;
+using Azure.AI.OpenAI;
 using ContosoSuitesWebAPI.Entities;
 using Microsoft.Azure.Cosmos;
 
@@ -27,7 +28,7 @@ namespace ContosoSuitesWebAPI.Services
                 var embeddings = await embeddingClient.GenerateEmbeddingAsync(text);
 
                 var vector = embeddings.Value.Vector.ToArray();
-
+                
                 // Return the vector embeddings.
                 return vector;
             }
@@ -42,28 +43,27 @@ namespace ContosoSuitesWebAPI.Services
         ///// Perform a vector search query against Cosmos DB.
         ///// This requires that you have already performed vectorization on your input text using the GetEmbeddings() method.
         ///// </summary>
-        //public async Task<List<VectorSearchResult>> ExecuteVectorSearch(float[] queryVector, int max_results = 0, double minimum_similarity_score = 0.8)
-        //{
-        //    var db = _cosmosClient.GetDatabase(configuration.GetValue<string>("CosmosDB:DatabaseName") ?? "ContosoSuites");
-        //    var container = db.GetContainer(configuration.GetValue<string>("CosmosDB:MaintenanceRequestsContainerName") ?? "MaintenanceRequests");
+        public async Task<List<VectorSearchResult>> ExecuteVectorSearch(float[] queryVector, int max_results = 0, double minimum_similarity_score = 0.8)
+        {
+            var db = _cosmosClient.GetDatabase(configuration.GetValue<string>("CosmosDB:DatabaseName") ?? "ContosoSuites");
+            var container = db.GetContainer(configuration.GetValue<string>("CosmosDB:MaintenanceRequestsContainerName") ?? "MaintenanceRequests");
+            var vectorString = string.Join(", ", queryVector.Select(v => v.ToString(CultureInfo.InvariantCulture)).ToArray());
 
-        //    var vectorString = string.Join(", ", queryVector.Select(v => v.ToString(CultureInfo.InvariantCulture)).ToArray());
+            var query = $"SELECT c.hotel_id AS HotelId, c.hotel AS Hotel, c.details AS Details, c.source AS Source, VectorDistance(c.request_vector, [{vectorString}]) AS SimilarityScore FROM c";
+            query += $" WHERE VectorDistance(c.request_vector, [{vectorString}]) > {minimum_similarity_score.ToString(CultureInfo.InvariantCulture)}";
+            query += $" ORDER BY VectorDistance(c.request_vector, [{vectorString}])";
 
-        //    var query = $"SELECT c.hotel_id AS HotelId, c.hotel AS Hotel, c.details AS Details, c.source AS Source, VectorDistance(c.request_vector, [{vectorString}]) AS SimilarityScore FROM c";
-        //    query += $" WHERE VectorDistance(c.request_vector, [{vectorString}]) > {minimum_similarity_score.ToString(CultureInfo.InvariantCulture)}";
-        //    query += $" ORDER BY VectorDistance(c.request_vector, [{vectorString}])";
+            var results = new List<VectorSearchResult>();
 
-        //    var results = new List<VectorSearchResult>();
-
-        //    using var feedIterator = container.GetItemQueryIterator<VectorSearchResult>(new QueryDefinition(query));
-        //    while (feedIterator.HasMoreResults)
-        //    {
-        //        foreach (var item in await feedIterator.ReadNextAsync())
-        //        {
-        //            results.Add(item);
-        //        }
-        //    }
-        //    return max_results > 0 ? results.Take(max_results).ToList() : [.. results];
-        //}
+            using var feedIterator = container.GetItemQueryIterator<VectorSearchResult>(new QueryDefinition(query));
+            while (feedIterator.HasMoreResults)
+            {
+                foreach (var item in await feedIterator.ReadNextAsync())
+                {
+                    results.Add(item);
+                }
+            }
+            return max_results > 0 ? results.Take(max_results).ToList() : [.. results];
+        }
     }
 }
